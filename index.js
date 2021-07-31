efc90ad2f861fddfff29c8f687fb10406fa55fd7
Toshihiro Sekioka <toshihiro_sekioka@yayoi-kk.co.jp>
2021-07-31 12:05:24 +0900

    formの表示機能を追加

--- index.js
+++ index.js
@@ -2,19 +2,28 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// ejsファイルのrenderに必要
 app.set("view engine", "ejs")
+// formの送信に必要
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     let name = req.query.name || "no data"
     let count = req.query.count || "no data"
     res.render("index", {
         count: count,
-        name: name
+        name: name,
+        address: "no data"
     })
 })
 
 app.post('/', (req, res) => {
-    res.render("index")
+    let address = req.body.address || "no data"
+    res.render("index", {
+        count: "no data",
+        name: "no data",
+        address: address
+    })
 })
 
 app.listen(port, () => {
