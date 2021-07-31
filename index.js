879b11554653b534e80beb36fde15f8bad09cdaf
Toshihiro Sekioka <toshihiro_sekioka@yayoi-kk.co.jp>
2021-07-31 12:40:45 +0900

    保存と表示を追加

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
 const express = require('express')
+const dateformat = require("dateformat")
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    let todoList = ["abc", "123"]
+app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: true }))
+
+// Sequelizeの設定
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: "./database.sqlite3"
+});
+
+// EJS内で使える関数を定義
+app.locals.dateformat = dateformat
+
+
+class Todo extends Model { }
+Todo.init({
+    todo: DataTypes.STRING,
+    created: DataTypes.DATE
+}, { sequelize, modelName: 'todo' });
+
+app.get('/', async (req, res) => {
+    let todoList = await Todo.findAll()
 
     res.render("index", {
         todoList: todoList
     })
 })
 
-app.post('/', (req, res) => {
-    let todoList = ["abc", "123"]
-    todoList.push(req.body.todo)
+app.post('/', async (req, res) => {
+    await Todo.create({
+        todo: req.body.todo,
+        created: Date()
+    })
+    let todoList = await Todo.findAll()
 
     res.render("index", {
         todoList: todoList
     })
 })
+
+// データベースファイルを作成する
+sequelize.sync()
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
